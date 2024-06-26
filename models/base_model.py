8054f43dd3871628b230e7bf2ffc3c4c2627a5f0
Bayelaye313 <bayelaye0510@gmail.com>
2024-06-26 21:15:28 +0000

    classdb with alchemy

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+from models import storage
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
 
+
+Base = declarative_base()
 
 class BaseModel:
+
+    """implementing attributes for our db"""
+    
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.now())
+    updated_at = Column(DateTime, nullable=False, default=created_at)
+
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
@@ -13,7 +25,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -29,8 +40,8 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -42,3 +53,8 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+
+    def delete(self):
+        """Delete current instance from storage
+        """
+        storage.delete(self)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """ State class
+        Attributes:
+        __tablename__: name of MySQL table
+        name: input name
+    """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+
